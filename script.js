063ae39572d51bdff06775c2bd1dfc276ff16eb1
rkzucco <xzucco@hotmail.com>
2023-05-02 20:55:05 -0300

    Upload da estrutura base da tela de login com inputs de e-mail e senha

---
+++ script.js
@@ -0,0 +1,8 @@
+let email = document.getElementById("campo-email")
+
+function enviarEmail(event) {
+    let emailDigitado = email.value
+    console.log(emailDigitado)
+    event.preventDefault()
+
+}
